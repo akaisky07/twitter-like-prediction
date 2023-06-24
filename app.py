8388fea9c71378a3eef10abc7451f3a34ba28aa6
Aman Kumar <104855741+akaisky07@users.noreply.github.com>
2023-06-24 09:39:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = joblib.load('twitter_prediction.pkl')
+
+# Load the CountVectorizer
+vectorizer = joblib.load('vectorizer.pkl')
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('index.html', prediction=None)
+
+# Prediction endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    tweet = request.form['tweet']
+    
+    # Preprocess the tweet using the loaded vectorizer
+    tweet_vectorized = vectorizer.transform([tweet]).astype(int)
+    
+    # Make the prediction using the pre-trained model
+    prediction = model.predict(tweet_vectorized)
+
+    return render_template('index.html', prediction=prediction[0]+1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
